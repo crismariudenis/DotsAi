344fe12e30a0869e48bf8500616684881a2c94ad
crismariudenis <78813212+crismariudenis@users.noreply.github.com>
2022-08-17 16:52:47 +0300

    adding comments for JavaDoc

--- src/main/java/com/example/Matrix.java
+++ src/main/java/com/example/Matrix.java
@@ -6,12 +6,22 @@ import java.util.Random;
 import static com.example.Main.*;
 import static com.example.Main.p;
 
+/**
+ * @author Denis Crismariu
+ */
 public class Matrix {
     float[][] w;
     int height;
     int width;
     float mini, maxi;
 
+    /**
+     * Generates the matrix of weights
+     * @param height the height of the matrix
+     * @param width the width of the matrix
+     * @param mini the min value in the matrix
+     * @param maxi the max value in the matrix
+     */
     Matrix(int height, int width, float mini, float maxi) {
         this.height = height;
         this.width = width;
@@ -22,6 +32,12 @@ public class Matrix {
     }
 
     //initialize the Matrix with random values
+
+    /**
+     * Initializing with random values
+     * @param mini the min value in the matrix
+     * @param maxi the max value in the matrix
+     */
     private void initialize(float mini, float maxi) {
         for (int i = 0; i < height; i++)
             for (int j = 0; j < width; j++) {
@@ -30,7 +46,9 @@ public class Matrix {
             }
     }
 
-    //print the Matrix
+    /**
+     * Function to print the matrix
+     */
     void print() {
         for (int i = 0; i < height; i++, System.out.println())
             for (int j = 0; j < width; j++) {
@@ -38,7 +56,12 @@ public class Matrix {
             }
     }
 
-    //calculate the output from every layer
+    /**
+     * Calculating the input to the next layer
+     * @param input input layer
+     * @param bias bias values for next level
+     * @return the output after the matrix multiplication
+     */
     public ArrayList<Float> calc(ArrayList<Float> input, ArrayList<Float> bias) {
         ArrayList<Float> ans = new ArrayList<>();
         if (height != input.size()) {
@@ -55,6 +78,9 @@ public class Matrix {
         return ans;
     }
 
+    /**
+     * Mutate the weights
+     */
     public void mutate() {
         for (int i = 0; i < w.length; i++)
             for (int j = 0; j < w[i].length; j++) {
@@ -65,8 +91,11 @@ public class Matrix {
                 }
             }
     }
-
-    //merge two matrices with 50%
+    /**
+     * Merge wro matrices with 50% each
+     * @param x the other matrix
+     * @return the combination of the Matrices
+     */
     public Matrix merge(Matrix x) {
         Matrix ans = this.copy();
 
@@ -79,6 +108,10 @@ public class Matrix {
         return ans;
     }
 
+    /**
+     * Creating a copy of the matrix
+     * @return the copy of the Matrix
+     */
     public Matrix copy() {
         Matrix clone = new Matrix(height, width, -100, 100);
         clone.width = this.width;
@@ -90,6 +123,12 @@ public class Matrix {
     }
 
     //compare the number of differences between two matrices
+
+    /**
+     * Returns the number of differences in matrix
+     * @param m the second matrix
+     * @return number of differences
+     */
     public int compare(Matrix m) {
         int ans = 0;
         for (int i = 0; i < height; i++)
@@ -99,6 +138,12 @@ public class Matrix {
         return ans;
     }
 
+    /**
+     * Function for accessing values from the matrix
+     * @param x coordinate
+     * @param y coordinate
+     * @return the values at that location
+     */
     public float get(int x, int y) {
         return w[x][y];
     }
